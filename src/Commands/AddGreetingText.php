ba1df890f876860bf224e1972653991c699e5abb
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-06-06 14:18:32 +0000

    Cập nhật src/Commands/AddGreetingText.php

--- src/Commands/AddGreetingText.php
+++ src/Commands/AddGreetingText.php
@@ -48,4 +48,5 @@ class AddGreetingText extends Command
 
         if (! $payload) {
             $this->error('You need to add a Facebook greeting text to your Bot Messenger Facebook config.');
-
+            exit;
+     * @return string|null
