0ab791f4ba326806d9b55139dab78cdb5f6039e2
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-06-18 23:35:50 +0000

    Cập nhật src/Commands/AddPersistentMenu.php

--- src/Commands/AddPersistentMenu.php
+++ src/Commands/AddPersistentMenu.php
@@ -54,4 +54,5 @@ class AddPersistentMenu extends Command
         $response = $this->http->post('https://graph.facebook.com/v2.6/me/messenger_profile?access_token='.config('facebook.token'),
             [], $payload);
 
-                    return (isset($attachment['type'])) && $attachment['type'] === 'location';
+        $responseObject = json_decode($response->getContent());
+     * @return $this
