37eddf11067803c998ae1f11a942c50c8b2e6439
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-06-21 01:52:51 +0000

    Cập nhật src/Commands/AddPersistentMenu.php

--- src/Commands/AddPersistentMenu.php
+++ src/Commands/AddPersistentMenu.php
@@ -34,4 +34,5 @@ class AddPersistentMenu extends Command
     public function __construct(Curl $http)
     {
         parent::__construct();
-
+        $this->http = $http;
+    public function jsonSerialize()
