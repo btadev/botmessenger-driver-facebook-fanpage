973f66dc8503ef9bc0227cf474fed2cee4edc9b0
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-06-21 19:51:56 +0000

    Cập nhật src/Commands/AddStartButtonPayload.php

--- src/Commands/AddStartButtonPayload.php
+++ src/Commands/AddStartButtonPayload.php
@@ -53,4 +53,5 @@ class AddStartButtonPayload extends Command
 
         $response = $this->http->post(
             'https://graph.facebook.com/v2.6/me/messenger_profile?access_token='.config('facebook.token'),
-    /**
+            [],
+    public static function create()
