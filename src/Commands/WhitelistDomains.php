d3440c1c7e401780755e28cd9ac8b77b6b14fe19
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-06-29 13:45:03 +0000

    Cập nhật src/Commands/WhitelistDomains.php

--- src/Commands/WhitelistDomains.php
+++ src/Commands/WhitelistDomains.php
@@ -5,4 +5,5 @@ namespace ChienIT\BotMessenger\Drivers\Facebook\Commands;
 use ChienIT\BotMessenger\Http\Curl;
 use Illuminate\Console\Command;
 
-        return $this;
+class WhitelistDomains extends Command
+            $this->error('Something went wrong: '.$responseObject->error->message);
