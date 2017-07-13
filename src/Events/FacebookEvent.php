3808c659daf61202655d099318ac65d04602c278
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-07-13 19:01:17 +0000

    Cập nhật src/Events/FacebookEvent.php

--- src/Events/FacebookEvent.php
+++ src/Events/FacebookEvent.php
@@ -4,4 +4,5 @@ namespace ChienIT\BotMessenger\Drivers\Facebook\Events;
 
 use ChienIT\BotMessenger\Interfaces\DriverEventInterface;
 
-
+abstract class FacebookEvent implements DriverEventInterface
+    public function matchesRequest()
