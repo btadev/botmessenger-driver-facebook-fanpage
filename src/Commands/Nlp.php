d04b89b7604c94977a5596d86044a8007ee70a34
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-07-16 06:42:02 +0000

    Cập nhật src/Commands/Nlp.php

--- src/Commands/Nlp.php
+++ src/Commands/Nlp.php
@@ -50,3 +50,5 @@ class Nlp extends Command
         $responseObject = json_decode($response->getContent());
 
         if ($response->getStatusCode() == 200) {
+            if ($this->option('disable')) {
+    public function getName()
