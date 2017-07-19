5e8dd701f3f163184f1252c93daaa7719fb0f052
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-07-19 18:03:27 +0000

    Cập nhật src/Commands/WhitelistDomains.php

--- src/Commands/WhitelistDomains.php
+++ src/Commands/WhitelistDomains.php
@@ -48,4 +48,5 @@ class WhitelistDomains extends Command
 
         if (! $payload) {
             $this->error('You need to add a Facebook whitelist to your Bot Messenger Facebook config.');
-
+            exit;
+     * @return array
