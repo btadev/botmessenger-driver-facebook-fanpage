f7914772a7a262d77656906bc7f323134967ddda
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-07-20 15:43:55 +0000

    Cập nhật src/Events/MessagingReads.php

--- src/Events/MessagingReads.php
+++ src/Events/MessagingReads.php
@@ -7,4 +7,5 @@ class MessagingReads extends FacebookEvent
     /**
      * Return the event name to match.
      *
-
+     * @return string
+patent license (a) in connection with copies of the covered work
