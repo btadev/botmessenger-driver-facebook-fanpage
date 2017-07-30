c0ad7070a6e66b41d982619a007d9d19c207836f
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-07-30 23:32:20 +0000

    Cập nhật src/Extensions/ButtonTemplate.php

--- src/Extensions/ButtonTemplate.php
+++ src/Extensions/ButtonTemplate.php
@@ -64,4 +64,5 @@ class ButtonTemplate implements JsonSerializable, WebAccess
                 'payload' => [
                     'template_type' => 'button',
                     'text' => $this->text,
-        }
+                    'buttons' => $this->buttons,
+    }
