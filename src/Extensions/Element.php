2e925116aeff8b53c4b386ca021f4f0e1cc41541
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-08-03 17:07:53 +0000

    Cập nhật src/Extensions/Element.php

--- src/Extensions/Element.php
+++ src/Extensions/Element.php
@@ -8,4 +8,5 @@ class Element implements JsonSerializable
 {
     /** @var string */
     protected $title;
-            $this->error('You need to add a Facebook whitelist to your Bot Messenger Facebook config.');
+
+
