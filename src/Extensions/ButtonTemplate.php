d92172f552e395e4a823e45919c092f863d21d04
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-08-16 23:00:17 +0000

    Cập nhật src/Extensions/ButtonTemplate.php

--- src/Extensions/ButtonTemplate.php
+++ src/Extensions/ButtonTemplate.php
@@ -81,4 +81,5 @@ class ButtonTemplate implements JsonSerializable, WebAccess
     /**
      * Get the instance as a web accessible array.
      * This will be used within the WebDriver.
-     * Get the instance as a web accessible array.
+     *
+        return $this->messages;
