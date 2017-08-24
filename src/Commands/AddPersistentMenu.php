5d7f55825e5c363c64cff159901d38fcb523c4be
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-08-24 00:40:02 +0000

    Cập nhật src/Commands/AddPersistentMenu.php

--- src/Commands/AddPersistentMenu.php
+++ src/Commands/AddPersistentMenu.php
@@ -51,3 +51,4 @@ class AddPersistentMenu extends Command
             exit;
         }
 
+        $response = $this->http->post('https://graph.facebook.com/v2.6/me/messenger_profile?access_token='.config('facebook.token'),
