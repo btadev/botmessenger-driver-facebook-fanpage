9e889f147410600d08ff6c70f54f899b7e401e37
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-08-25 15:52:28 +0000

    Cập nhật src/Commands/AddStartButtonPayload.php

--- src/Commands/AddStartButtonPayload.php
+++ src/Commands/AddStartButtonPayload.php
@@ -55,3 +55,4 @@ class AddStartButtonPayload extends Command
             'https://graph.facebook.com/v2.6/me/messenger_profile?access_token='.config('facebook.token'),
             [],
             [
+                'get_started' => [
