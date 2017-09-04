11edb01a7dede0114abee1a91c93e4172a536f78
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-09-04 09:45:42 +0000

    Cập nhật src/Commands/AddPersistentMenu.php

--- src/Commands/AddPersistentMenu.php
+++ src/Commands/AddPersistentMenu.php
@@ -49,3 +49,4 @@ class AddPersistentMenu extends Command
         if (! $payload) {
             $this->error('You need to add a Facebook menu payload data to your Bot Messenger Facebook config.');
             exit;
+        }
