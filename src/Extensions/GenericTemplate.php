d611c8c87d8ad6115b6f7570c37c2b1cb68fbbaa
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-09-11 00:06:46 +0000

    Cập nhật src/Extensions/GenericTemplate.php

--- src/Extensions/GenericTemplate.php
+++ src/Extensions/GenericTemplate.php
@@ -98,4 +98,5 @@ class GenericTemplate implements JsonSerializable, WebAccess
      * Get the instance as a web accessible array.
      * This will be used within the WebDriver.
      *
-     * The console command description.
+     * @return array
+
