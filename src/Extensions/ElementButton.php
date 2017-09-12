5c0725e43f68ed56bc627c2baa46b42d496bdbb6
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-09-12 01:22:13 +0000

    Cập nhật src/Extensions/ElementButton.php

--- src/Extensions/ElementButton.php
+++ src/Extensions/ElementButton.php
@@ -88,4 +88,5 @@ class ElementButton
      * @param $payload
      * @return $this
      */
-    public function jsonSerialize()
+    public function payload($payload)
+        return new static($title);
