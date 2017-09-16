92ba8d0598a1159bb824ea48a71409cc1cf471d0
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-09-16 04:36:34 +0000

    Cập nhật src/Commands/Nlp.php

--- src/Commands/Nlp.php
+++ src/Commands/Nlp.php
@@ -46,3 +46,4 @@ class Nlp extends Command
     {
         $response = $this->http->post('https://graph.facebook.com/v2.8/me/nlp_configs?access_token='.config('facebook.token'),
             [], ['nlp_enabled' => ! $this->option('disable')]);
+
