6fd746832b103551f2ebdfa61e73817c711fae08
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-09-30 17:00:33 +0000

    Cập nhật src/Extensions/GenericTemplate.php

--- src/Extensions/GenericTemplate.php
+++ src/Extensions/GenericTemplate.php
@@ -81,4 +81,5 @@ class GenericTemplate implements JsonSerializable, WebAccess
                     'template_type' => 'generic',
                     'image_aspect_ratio' => $this->imageAspectRatio,
                     'elements' => $this->elements,
-     *
+                ],
+    /**
