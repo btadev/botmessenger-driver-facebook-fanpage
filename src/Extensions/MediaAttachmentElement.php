419c935dbbd50f80b408a8ac08688f2e99ec99d7
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-10-02 17:34:57 +0000

    Cập nhật src/Extensions/MediaAttachmentElement.php

--- src/Extensions/MediaAttachmentElement.php
+++ src/Extensions/MediaAttachmentElement.php
@@ -20,4 +20,4 @@ class MediaAttachmentElement implements JsonSerializable
      * @return static
      */
     public static function create($mediaType)
-        $messages = Collection::make($this->event->get('messaging'))->filter(function ($msg) {
+    {
