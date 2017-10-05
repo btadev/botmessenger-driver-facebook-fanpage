9424b85ea8365bec6e011fd8244beb4d737670ff
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-10-05 07:16:52 +0000

    Cập nhật src/Extensions/MediaAttachmentElement.php

--- src/Extensions/MediaAttachmentElement.php
+++ src/Extensions/MediaAttachmentElement.php
@@ -4,4 +4,4 @@ namespace ChienIT\BotMessenger\Drivers\Facebook\Extensions;
 
 use JsonSerializable;
 
-namespace ChienIT\BotMessenger\Drivers\Facebook\Extensions;
+class MediaAttachmentElement implements JsonSerializable
