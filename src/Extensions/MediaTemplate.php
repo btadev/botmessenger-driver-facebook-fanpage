6b0d2e9684cea4309c8d0c82187a1085be349b66
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-10-13 14:57:19 +0000

    Cập nhật src/Extensions/MediaTemplate.php

--- src/Extensions/MediaTemplate.php
+++ src/Extensions/MediaTemplate.php
@@ -5,4 +5,5 @@ namespace ChienIT\BotMessenger\Drivers\Facebook\Extensions;
 use JsonSerializable;
 use ChienIT\BotMessenger\Interfaces\WebAccess;
 
-        }
+class MediaTemplate implements JsonSerializable, WebAccess
+    public function orderUrl($orderUrl)
