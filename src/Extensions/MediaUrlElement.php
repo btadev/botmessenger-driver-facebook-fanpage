1e5aac5b1d515e4d7d2a8cfb25e5724c164119d6
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-11-03 23:14:09 +0000

    Cập nhật src/Extensions/MediaUrlElement.php

--- src/Extensions/MediaUrlElement.php
+++ src/Extensions/MediaUrlElement.php
@@ -62,4 +62,5 @@ class MediaUrlElement implements JsonSerializable
     {
         foreach ($buttons as $button) {
             if ($button instanceof ElementButton) {
-
+                $this->buttons[] = $button->toArray();
+<?php
