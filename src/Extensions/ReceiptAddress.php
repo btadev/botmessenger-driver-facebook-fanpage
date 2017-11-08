93f4a0d11a37fa552b1c6ce485777e41bc80dd87
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-11-08 17:05:45 +0000

    Cập nhật src/Extensions/ReceiptAddress.php

--- src/Extensions/ReceiptAddress.php
+++ src/Extensions/ReceiptAddress.php
@@ -74,4 +74,5 @@ class ReceiptAddress implements JsonSerializable
         $this->postal_code = $postalCode;
 
         return $this;
-        $responseObject = json_decode($response->getContent());
+    }
+
