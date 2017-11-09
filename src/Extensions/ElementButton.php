eb0aa3182be838b151b8a6aeee4ed63bb24fdfde
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-11-09 14:02:06 +0000

    Cập nhật src/Extensions/ElementButton.php

--- src/Extensions/ElementButton.php
+++ src/Extensions/ElementButton.php
@@ -183,3 +183,4 @@ class ElementButton
                     $buttonArray['messenger_extensions'] = $this->messenger_extensions;
                     $buttonArray['fallback_url'] = $this->fallback_url ?: $this->url;
                 }
+            }
