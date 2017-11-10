5d22392a3d502c94796ca398de9032d617177336
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-11-10 22:01:08 +0000

    Cập nhật src/Extensions/ElementButton.php

--- src/Extensions/ElementButton.php
+++ src/Extensions/ElementButton.php
@@ -143,3 +143,4 @@ class ElementButton
      * Optional. The message that you wish the recipient of the share to see,
      * if it is different from the one this button is attached to.
      * The format follows that used in Send API, but must be a generic template with up to one URL button.
+     * @param GenericTemplate $shareContents
