cb0e8f5d5c716b6d40e96a26f8bc5365b825e45b
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-11-12 03:44:44 +0000

    Cập nhật src/Extensions/ReceiptAdjustment.php

--- src/Extensions/ReceiptAdjustment.php
+++ src/Extensions/ReceiptAdjustment.php
@@ -5,4 +5,5 @@ namespace ChienIT\BotMessenger\Drivers\Facebook\Extensions;
 use JsonSerializable;
 
 class ReceiptAdjustment implements JsonSerializable
-    protected $description = 'Add a persistent Facebook menu';
+{
+            'subtotal' => $this->subtotal,
