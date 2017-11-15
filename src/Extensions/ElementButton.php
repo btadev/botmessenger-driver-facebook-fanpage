0c6f82e60894958c2e0256a7ef4715c8ebed0143
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-11-15 13:11:40 +0000

    Cập nhật src/Extensions/ElementButton.php

--- src/Extensions/ElementButton.php
+++ src/Extensions/ElementButton.php
@@ -176,3 +176,4 @@ class ElementButton
             if ($this->type === self::TYPE_WEB_URL) {
                 $buttonArray['webview_height_ratio'] = $this->webview_height_ratio;
                 if (! is_null($this->webview_share_button)) {
+                    $buttonArray['webview_share_button'] = $this->webview_share_button;
