a0a8df0b6facb736efecbaa04681021f6f2bba3e
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-11-19 01:16:27 +0000

    Cập nhật src/Extensions/ElementButton.php

--- src/Extensions/ElementButton.php
+++ src/Extensions/ElementButton.php
@@ -165,3 +165,4 @@ class ElementButton
         if ($this->type !== self::TYPE_SHARE) {
             if ($this->type !== self::TYPE_ACCOUNT_LINK && $this->type !== self::TYPE_ACCOUNT_UNLINK) {
                 $buttonArray['title'] = $this->title;
+            }
