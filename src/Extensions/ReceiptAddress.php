de09cac9b16e39037019777a0d32ad5fbb078ed3
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-11-22 04:31:05 +0000

    Cập nhật src/Extensions/ReceiptAddress.php

--- src/Extensions/ReceiptAddress.php
+++ src/Extensions/ReceiptAddress.php
@@ -5,4 +5,5 @@ namespace ChienIT\BotMessenger\Drivers\Facebook\Extensions;
 use JsonSerializable;
 
 class ReceiptAddress implements JsonSerializable
-    /** @var string */
+{
+     * @param string $title
