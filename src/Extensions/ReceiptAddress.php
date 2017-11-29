60c13a7c28f2c6dd713c910f099328a6bc57e70f
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-11-29 22:16:10 +0000

    Cập nhật src/Extensions/ReceiptAddress.php

--- src/Extensions/ReceiptAddress.php
+++ src/Extensions/ReceiptAddress.php
@@ -105,4 +105,5 @@ class ReceiptAddress implements JsonSerializable
     {
         return [
             'street_1' => $this->street_1,
-    /**
+            'street_2' => $this->street_2,
+     * Return the event name to match.
