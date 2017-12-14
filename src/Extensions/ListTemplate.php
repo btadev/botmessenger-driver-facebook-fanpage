02ae1868d22c21c80d07ac6d1e064c3537c670bd
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-12-14 17:33:41 +0000

    Cập nhật src/Extensions/ListTemplate.php

--- src/Extensions/ListTemplate.php
+++ src/Extensions/ListTemplate.php
@@ -110,3 +110,4 @@ class ListTemplate implements JsonSerializable, WebAccess
         return [
             'type' => 'list',
             'elements' => $this->elements,
+            'globalButtons' => [$this->globalButton],
