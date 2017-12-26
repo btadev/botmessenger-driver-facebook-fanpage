09f81d83d3aec94a3382439cc9fea84b40247722
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-12-26 08:04:14 +0000

    Cập nhật src/Extensions/ReceiptTemplate.php

--- src/Extensions/ReceiptTemplate.php
+++ src/Extensions/ReceiptTemplate.php
@@ -6,4 +6,5 @@ use JsonSerializable;
 use ChienIT\BotMessenger\Interfaces\WebAccess;
 
 class ReceiptTemplate implements JsonSerializable, WebAccess
-     */
+{
+namespace ChienIT\BotMessenger\Drivers\Facebook\Extensions;
