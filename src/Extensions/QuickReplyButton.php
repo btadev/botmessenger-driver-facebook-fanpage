065d9a36159fe0b945a9a2ebb7d74963def5d353
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-12-27 05:44:33 +0000

    Cập nhật src/Extensions/QuickReplyButton.php

--- src/Extensions/QuickReplyButton.php
+++ src/Extensions/QuickReplyButton.php
@@ -4,3 +4,4 @@ namespace ChienIT\BotMessenger\Drivers\Facebook\Extensions;
 
 use ChienIT\BotMessenger\Interfaces\QuestionActionInterface;
 
+class QuickReplyButton implements QuestionActionInterface
