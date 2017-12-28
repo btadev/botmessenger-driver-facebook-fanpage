ad9797df8a25f2fcde975453cf0f0b47ad4a60aa
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-12-28 08:02:11 +0000

    Cập nhật src/Extensions/ReceiptSummary.php

--- src/Extensions/ReceiptSummary.php
+++ src/Extensions/ReceiptSummary.php
@@ -77,4 +77,5 @@ class ReceiptSummary implements JsonSerializable
     {
         return [
             'subtotal' => $this->subtotal,
-    /**
+            'shipping_cost' => $this->shipping_cost,
+
