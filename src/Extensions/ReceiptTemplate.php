b15b8955dfd07a7ce0323c7758b5d8a4eeae2942
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-12-30 06:08:37 +0000

    Cập nhật src/Extensions/ReceiptTemplate.php

--- src/Extensions/ReceiptTemplate.php
+++ src/Extensions/ReceiptTemplate.php
@@ -155,4 +155,5 @@ class ReceiptTemplate implements JsonSerializable, WebAccess
      * @param ReceiptAddress $address
      * @return $this
      */
-        $this->elements = [$element->toArray()];
+    public function addAddress(ReceiptAddress $address)
+     */
