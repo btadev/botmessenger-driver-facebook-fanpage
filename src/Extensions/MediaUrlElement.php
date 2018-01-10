b2c31a4a1acc23e7f4ff77d794fea94dfcc5817e
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-01-10 08:10:52 +0000

    Cập nhật src/Extensions/MediaUrlElement.php

--- src/Extensions/MediaUrlElement.php
+++ src/Extensions/MediaUrlElement.php
@@ -64,3 +64,4 @@ class MediaUrlElement implements JsonSerializable
             if ($button instanceof ElementButton) {
                 $this->buttons[] = $button->toArray();
             }
+        }
