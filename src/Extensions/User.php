6b4952b028a01d015a65a83e4268808b32bfe70e
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-01-16 13:06:35 +0000

    Cập nhật src/Extensions/User.php

--- src/Extensions/User.php
+++ src/Extensions/User.php
@@ -5,4 +5,5 @@ namespace ChienIT\BotMessenger\Drivers\Facebook\Extensions;
 use ChienIT\BotMessenger\Interfaces\UserInterface;
 use ChienIT\BotMessenger\Users\User as BotMessengerUser;
 
-
+class User extends BotMessengerUser implements UserInterface
+        $payload = config('facebook.whitelisted_domains');
