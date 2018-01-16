e5569912f90c468420890e24bbacda5affc466ba
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-01-16 18:07:06 +0000

    Cập nhật src/Extensions/QuickReplyButton.php

--- src/Extensions/QuickReplyButton.php
+++ src/Extensions/QuickReplyButton.php
@@ -83,3 +83,4 @@ class QuickReplyButton implements QuestionActionInterface
 
         if ($this->contentType === 'text') {
             $buttonArray = [
+                'content_type' => $this->contentType,
