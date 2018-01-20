3a3838c0f154ed6a82141d7aa34ad5b7d148247b
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-01-20 14:11:40 +0000

    Cập nhật src/FacebookAudioDriver.php

--- src/FacebookAudioDriver.php
+++ src/FacebookAudioDriver.php
@@ -18,4 +18,5 @@ class FacebookAudioDriver extends FacebookDriver
     public function matchesRequest()
     {
         $validSignature = ! $this->config->has('facebook_app_secret') || $this->validateSignature();
-     */
+        $messages = Collection::make($this->event->get('messaging'))->filter(function ($msg) {
+{
