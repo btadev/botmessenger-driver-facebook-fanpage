422f88171a163fc580ca5d0f940e364092e8804d
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-01-22 14:05:17 +0000

    Cập nhật src/FacebookDriver.php

--- src/FacebookDriver.php
+++ src/FacebookDriver.php
@@ -12,4 +12,5 @@ use ChienIT\BotMessenger\Messages\Attachments\Audio;
 use ChienIT\BotMessenger\Messages\Attachments\Image;
 use ChienIT\BotMessenger\Messages\Attachments\Video;
 use ChienIT\BotMessenger\Messages\Outgoing\Question;
-     *
+use Symfony\Component\HttpFoundation\Request;
+    {
