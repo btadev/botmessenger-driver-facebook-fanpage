6fa387cc82f16a4766c7cf43df96e1787c8fba05
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-01-26 07:01:15 +0000

    Cập nhật src/FacebookAudioDriver.php

--- src/FacebookAudioDriver.php
+++ src/FacebookAudioDriver.php
@@ -6,3 +6,4 @@ use Illuminate\Support\Collection;
 use ChienIT\BotMessenger\Messages\Attachments\Audio;
 use ChienIT\BotMessenger\Messages\Incoming\IncomingMessage;
 
+        foreach ($buttons as $button) {
