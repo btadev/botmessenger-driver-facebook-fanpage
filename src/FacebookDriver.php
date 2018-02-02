e8a85470b17d1bef5b83672f1467f90bed4d1aa5
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-02-02 01:46:07 +0000

    Cập nhật src/FacebookDriver.php

--- src/FacebookDriver.php
+++ src/FacebookDriver.php
@@ -69,3 +69,5 @@ class FacebookDriver extends HttpDriver implements VerifiesService
     protected $facebookProfileEndpoint = 'https://graph.facebook.com/v2.6/';
 
     /** @var bool If the incoming request is a FB postback */
+    protected $isPostback = false;
+            $this->info('Domains where whitelisted.');
