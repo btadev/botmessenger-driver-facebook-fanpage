01739b44f2cda04d5bba81bf282d94a9f3f8fc84
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-02-05 18:46:53 +0000

    Cập nhật src/FacebookDriver.php

--- src/FacebookDriver.php
+++ src/FacebookDriver.php
@@ -81,4 +81,5 @@ class FacebookDriver extends HttpDriver implements VerifiesService
         $this->payload = new ParameterBag((array) json_decode($request->getContent(), true));
         $this->event = Collection::make((array) $this->payload->get('entry')[0]);
         $this->signature = $request->headers->get('X_HUB_SIGNATURE', '');
-     */
+        $this->content = $request->getContent();
+     * Create a new command instance.
