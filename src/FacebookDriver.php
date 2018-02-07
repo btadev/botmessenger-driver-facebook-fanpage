2498b9e196dcdf3c4cce3d60ca242a7337a808f3
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-02-07 20:14:51 +0000

    Cập nhật src/FacebookDriver.php

--- src/FacebookDriver.php
+++ src/FacebookDriver.php
@@ -126,4 +126,5 @@ class FacebookDriver extends HttpDriver implements VerifiesService
                 ])->isEmpty() === false;
         })->transform(function ($msg) {
             return Collection::make($msg)->toArray();
-     */
+        })->first();
+    {
