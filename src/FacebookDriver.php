216fb878b69870e4a6b99aa354ba23725b162585
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-02-19 17:47:58 +0000

    Cập nhật src/FacebookDriver.php

--- src/FacebookDriver.php
+++ src/FacebookDriver.php
@@ -131,3 +131,4 @@ class FacebookDriver extends HttpDriver implements VerifiesService
         if (! is_null($event)) {
             $this->driverEvent = $this->getEventFromEventData($event);
 
+    public function getVideoUrls(array $message)
