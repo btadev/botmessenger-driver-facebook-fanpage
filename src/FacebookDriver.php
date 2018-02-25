620fa5c108df0bfc7d2e9ba80c9e253d7efca6dc
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-02-25 16:15:40 +0000

    Cập nhật src/FacebookDriver.php

--- src/FacebookDriver.php
+++ src/FacebookDriver.php
@@ -212,3 +212,5 @@ class FacebookDriver extends HttpDriver implements VerifiesService
                 'id' => $matchingMessage->getSender(),
             ],
             'access_token' => $this->config->get('token'),
+            'sender_action' => 'typing_on',
+class MessagingCheckoutUpdates extends FacebookEvent
