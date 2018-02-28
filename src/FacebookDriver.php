54b8aa9f2b9260ab1bb54dc5a26f723b7c09d748
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-02-28 18:49:48 +0000

    Cập nhật src/FacebookDriver.php

--- src/FacebookDriver.php
+++ src/FacebookDriver.php
@@ -295,4 +295,5 @@ class FacebookDriver extends HttpDriver implements VerifiesService
      * Tells if the current request is a callback.
      *
      * @return bool
-     * @return bool
+     */
+     */
