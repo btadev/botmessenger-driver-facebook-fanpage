d783657ad92172fba24004d0bd743394da4953cd
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-03-05 09:42:30 +0000

    Cập nhật src/FacebookDriver.php

--- src/FacebookDriver.php
+++ src/FacebookDriver.php
@@ -265,3 +265,4 @@ class FacebookDriver extends HttpDriver implements VerifiesService
             } elseif (isset($msg['postback']['payload'])) {
                 $this->isPostback = true;
 
+        if ($this->type !== self::TYPE_SHARE) {
