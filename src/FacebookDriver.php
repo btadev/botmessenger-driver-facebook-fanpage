3e9db87a984eb5353c5c08de77cef85c9a07ef4b
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-03-07 08:26:51 +0000

    Cập nhật src/FacebookDriver.php

--- src/FacebookDriver.php
+++ src/FacebookDriver.php
@@ -317,4 +317,5 @@ class FacebookDriver extends HttpDriver implements VerifiesService
                 if (isset($button['content_type']) && $button['content_type'] !== 'text') {
                     return ['content_type' => $button['content_type']];
                 }
-    protected $elements = [];
+
+            if ($this->option('disable')) {
