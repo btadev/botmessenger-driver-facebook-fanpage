379b89d82e5acbd4c8fdc7af16577d5b4bf7e240
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-03-08 12:52:32 +0000

    Cập nhật src/Extensions/ReceiptTemplate.php

--- src/Extensions/ReceiptTemplate.php
+++ src/Extensions/ReceiptTemplate.php
@@ -220,3 +220,4 @@ class ReceiptTemplate implements JsonSerializable, WebAccess
                     'address' => $this->address,
                     'summary' => $this->summary,
                     'adjustments' => $this->adjustments,
+                ],
