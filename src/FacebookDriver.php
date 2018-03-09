d72ead249234a23e11fbae8021a63e7cd82b4038
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-03-09 04:49:38 +0000

    Cập nhật src/FacebookDriver.php

--- src/FacebookDriver.php
+++ src/FacebookDriver.php
@@ -268,4 +268,5 @@ class FacebookDriver extends HttpDriver implements VerifiesService
                 $message->setText($msg['postback']['payload']);
             } elseif (isset($msg['message']['quick_reply']['payload'])) {
                 $this->isPostback = true;
+
      */
