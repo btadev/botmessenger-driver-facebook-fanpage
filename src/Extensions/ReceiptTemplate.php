d52f657d2d3bfb7bd60391f8ef15b23d17e48762
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-03-11 09:36:51 +0000

    Cập nhật src/Extensions/ReceiptTemplate.php

--- src/Extensions/ReceiptTemplate.php
+++ src/Extensions/ReceiptTemplate.php
@@ -179,3 +179,4 @@ class ReceiptTemplate implements JsonSerializable, WebAccess
      */
     public function addAdjustment(ReceiptAdjustment $adjustment)
     {
+        $this->adjustments[] = $adjustment->toArray();
