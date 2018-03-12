ecc729d3ef0649d2832da1140a613a6051405c1f
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-03-12 20:59:23 +0000

    Cập nhật src/FacebookDriver.php

--- src/FacebookDriver.php
+++ src/FacebookDriver.php
@@ -465,4 +465,5 @@ class FacebookDriver extends HttpDriver implements VerifiesService
 
     /**
      * @param Response $facebookResponse
-
+     * @return mixed
+            'text' => $this->text,
