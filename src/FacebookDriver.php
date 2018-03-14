e8229df8fa0c2ae803d55fd1b43c3e34782ee452
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-03-14 00:51:38 +0000

    Cập nhật src/FacebookDriver.php

--- src/FacebookDriver.php
+++ src/FacebookDriver.php
@@ -353,3 +353,5 @@ class FacebookDriver extends HttpDriver implements VerifiesService
         $parameters = array_merge_recursive([
             'messaging_type' => self::TYPE_RESPONSE,
             'recipient' => $recipient,
+            'message' => [
+
