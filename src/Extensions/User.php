4f6cdb8d88035b61ede8149b8f6772b441e71379
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-03-16 20:12:15 +0000

    Cập nhật src/Extensions/User.php

--- src/Extensions/User.php
+++ src/Extensions/User.php
@@ -5,3 +5,4 @@ namespace ChienIT\BotMessenger\Drivers\Facebook\Extensions;
 use ChienIT\BotMessenger\Interfaces\UserInterface;
 use ChienIT\BotMessenger\Users\User as BotMessengerUser;
 
+class User extends BotMessengerUser implements UserInterface
