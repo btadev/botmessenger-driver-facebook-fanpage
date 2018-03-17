e9d924f8fded5b2e5fcf8ee2abff6e038c58fa0d
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-03-17 00:42:26 +0000

    Cập nhật src/FacebookDriver.php

--- src/FacebookDriver.php
+++ src/FacebookDriver.php
@@ -394,3 +394,5 @@ class FacebookDriver extends HttpDriver implements VerifiesService
     public function sendPayload($payload)
     {
         $response = $this->http->post($this->facebookProfileEndpoint.'me/messages', [], $payload);
+        $this->throwExceptionIfResponseNotOk($response);
+     * @return $this
