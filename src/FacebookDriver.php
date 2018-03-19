cc0474e7145cc90dc0bcb8fecb8d997d8f5bb34e
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-03-19 13:40:20 +0000

    Cập nhật src/FacebookDriver.php

--- src/FacebookDriver.php
+++ src/FacebookDriver.php
@@ -495,3 +495,5 @@ class FacebookDriver extends HttpDriver implements VerifiesService
      */
     protected function getMessageRecipient($msg)
     {
+        if (isset($msg['recipient'])) {
+    /**
