b74ce2bcb8dcc23b1c1c93e9b11c76998bf7899c
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-03-22 09:35:21 +0000

    Cập nhật src/Extensions/User.php

--- src/Extensions/User.php
+++ src/Extensions/User.php
@@ -22,3 +22,4 @@ class User extends BotMessengerUser implements UserInterface
         $this->id = $id;
         $this->first_name = $first_name;
         $this->last_name = $last_name;
+        $this->username = $username;
