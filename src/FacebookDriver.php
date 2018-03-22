a741e68d35f214522b897222a9580d5101f9dcab
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-03-22 13:16:13 +0000

    Cập nhật src/FacebookDriver.php

--- src/FacebookDriver.php
+++ src/FacebookDriver.php
@@ -429,4 +429,5 @@ class FacebookDriver extends HttpDriver implements VerifiesService
 
         $userInfoData = $this->http->get($this->facebookProfileEndpoint.$matchingMessage->getSender().'?fields='.$fields.'&access_token='.$this->config->get('token'));
 
-    }
+        $this->throwExceptionIfResponseNotOk($userInfoData);
+        $this->default_action = $defaultAction->toArray();
