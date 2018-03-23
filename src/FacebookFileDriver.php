def3d6e36ec970208e8676867359de05d217d995
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-03-23 15:36:28 +0000

    Cập nhật src/FacebookFileDriver.php

--- src/FacebookFileDriver.php
+++ src/FacebookFileDriver.php
@@ -53,4 +53,4 @@ class FacebookFileDriver extends FacebookDriver
         $messages = Collection::make($this->event->get('messaging'))->filter(function ($msg) {
             return isset($msg['message']) && isset($msg['message']['attachments']) && isset($msg['message']['attachments']);
         })->transform(function ($msg) {
-}
+            $message = new IncomingMessage(File::PATTERN, $msg['sender']['id'], $msg['recipient']['id'], $msg);
