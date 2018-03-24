10cb524b0d196a325465578015f4cc9814ed52cb
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-03-24 17:50:53 +0000

    Cập nhật src/FacebookDriver.php

--- src/FacebookDriver.php
+++ src/FacebookDriver.php
@@ -435,4 +435,4 @@ class FacebookDriver extends HttpDriver implements VerifiesService
         $firstName = $userInfo['first_name'] ?? null;
         $lastName = $userInfo['last_name'] ?? null;
 
-    protected $subtitle;
+        return new User($matchingMessage->getSender(), $firstName, $lastName, null, $userInfo);
