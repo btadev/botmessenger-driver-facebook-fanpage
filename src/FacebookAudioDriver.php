c4cb6bfff393410c546bb220373a1ab11291360f
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-03-25 23:56:47 +0000

    Cập nhật src/FacebookAudioDriver.php

--- src/FacebookAudioDriver.php
+++ src/FacebookAudioDriver.php
@@ -22,3 +22,4 @@ class FacebookAudioDriver extends FacebookDriver
             if (isset($msg['message']) && isset($msg['message']['attachments']) && isset($msg['message']['attachments'])) {
                 return Collection::make($msg['message']['attachments'])->filter(function ($attachment) {
                     return (isset($attachment['type'])) && $attachment['type'] === 'audio';
+                })->isEmpty() === false;
