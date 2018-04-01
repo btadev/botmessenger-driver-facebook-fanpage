96fbe9d9a28f92fa9619ec01e2cd992c98aac083
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-04-01 10:12:40 +0000

    Cập nhật src/Extensions/User.php

--- src/Extensions/User.php
+++ src/Extensions/User.php
@@ -54,3 +54,4 @@ class User extends BotMessengerUser implements UserInterface
      */
     public function getTimezone()
     {
+        return isset($this->user_info['timezone']) ? $this->user_info['timezone'] : null;
