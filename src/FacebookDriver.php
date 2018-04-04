c205b21a00cffc704ea4eef46604177cc37c1f69
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-04-04 12:59:15 +0000

    Cập nhật src/FacebookDriver.php

--- src/FacebookDriver.php
+++ src/FacebookDriver.php
@@ -19,3 +19,4 @@ use ChienIT\BotMessenger\Interfaces\DriverEventInterface;
 use ChienIT\BotMessenger\Drivers\Facebook\Events\MessagingReads;
 use Symfony\Component\HttpFoundation\ParameterBag;
 use ChienIT\BotMessenger\Drivers\Facebook\Events\MessagingOptins;
+use ChienIT\BotMessenger\Messages\Incoming\IncomingMessage;
