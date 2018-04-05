c0518d5bf722b325c2c149c91a749ac0be996ea7
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-04-05 23:46:31 +0000

    Cập nhật src/FacebookImageDriver.php

--- src/FacebookImageDriver.php
+++ src/FacebookImageDriver.php
@@ -63,3 +63,5 @@ class FacebookImageDriver extends FacebookDriver
             $messages = [new IncomingMessage('', '', '')];
         }
 
+        $this->messages = $messages;
+    protected $subtotal;
