b7152363d17f21106233c7a59289151fd591fba8
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-04-08 07:00:26 +0000

    Cập nhật src/FacebookDriver.php

--- src/FacebookDriver.php
+++ src/FacebookDriver.php
@@ -28,3 +28,4 @@ use ChienIT\BotMessenger\Drivers\Facebook\Extensions\ButtonTemplate;
 use ChienIT\BotMessenger\Drivers\Facebook\Events\MessagingDeliveries;
 use ChienIT\BotMessenger\Drivers\Facebook\Extensions\GenericTemplate;
 use ChienIT\BotMessenger\Drivers\Facebook\Extensions\ReceiptTemplate;
+use ChienIT\BotMessenger\Drivers\Facebook\Exceptions\FacebookException;
