beb7c26e1049a3d0c43105f8b76cf42b63db656c
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-04-13 12:35:25 +0000

    Cập nhật src/FacebookAudioDriver.php

--- src/FacebookAudioDriver.php
+++ src/FacebookAudioDriver.php
@@ -77,3 +77,4 @@ class FacebookAudioDriver extends FacebookDriver
         return Collection::make($message['message']['attachments'])->where('type',
             'audio')->pluck('payload')->map(function ($item) {
                 return new Audio($item['url'], $item);
+            })->toArray();
