51718a3822e0bb1476ee0132882e00c2f2259503
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-04-14 19:15:01 +0000

    Cập nhật src/FacebookImageDriver.php

--- src/FacebookImageDriver.php
+++ src/FacebookImageDriver.php
@@ -4,3 +4,5 @@ namespace ChienIT\BotMessenger\Drivers\Facebook;
 
 use Illuminate\Support\Collection;
 use ChienIT\BotMessenger\Messages\Attachments\Image;
+use ChienIT\BotMessenger\Messages\Incoming\IncomingMessage;
+     * The name and signature of the console command.
