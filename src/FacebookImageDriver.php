a79d89e9426b70ed6f8add22757b42a5a68eb083
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-04-16 20:48:01 +0000

    Cập nhật src/FacebookImageDriver.php

--- src/FacebookImageDriver.php
+++ src/FacebookImageDriver.php
@@ -72,4 +72,4 @@ class FacebookImageDriver extends FacebookDriver
      * @param array $message
      * @return array A download for the image file.
      */
-
+    public function getImagesUrls(array $message)
