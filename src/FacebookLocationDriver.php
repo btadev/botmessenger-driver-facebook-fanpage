9e36e92a22c8d0f6218f20aceff99e0ccb92d790
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-04-16 01:45:41 +0000

    Cập nhật src/FacebookLocationDriver.php

--- src/FacebookLocationDriver.php
+++ src/FacebookLocationDriver.php
@@ -18,4 +18,5 @@ class FacebookLocationDriver extends FacebookDriver
     public function matchesRequest()
     {
         $validSignature = ! $this->config->has('facebook_app_secret') || $this->validateSignature();
-     */
+        $messages = Collection::make($this->event->get('messaging'))->filter(function ($msg) {
+     * @var string
