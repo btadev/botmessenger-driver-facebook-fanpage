690ce6832a8366ccc8dbdd09145a998bd3c0fe1e
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-04-18 21:24:27 +0000

    Cập nhật src/FacebookDriver.php

--- src/FacebookDriver.php
+++ src/FacebookDriver.php
@@ -79,3 +79,4 @@ class FacebookDriver extends HttpDriver implements VerifiesService
     public function buildPayload(Request $request)
     {
         $this->payload = new ParameterBag((array) json_decode($request->getContent(), true));
+        $this->event = Collection::make((array) $this->payload->get('entry')[0]);
