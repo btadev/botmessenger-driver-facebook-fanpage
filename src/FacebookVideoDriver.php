6a234792229bad046a2bdd025ccd0f42d281b556
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-04-21 13:25:50 +0000

    Cập nhật src/FacebookVideoDriver.php

--- src/FacebookVideoDriver.php
+++ src/FacebookVideoDriver.php
@@ -6,3 +6,5 @@ use Illuminate\Support\Collection;
 use ChienIT\BotMessenger\Messages\Attachments\Video;
 use ChienIT\BotMessenger\Messages\Incoming\IncomingMessage;
 
+class FacebookVideoDriver extends FacebookDriver
+            'message' => [
