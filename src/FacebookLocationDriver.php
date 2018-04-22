a5a99ff527054a27aab332ce3b6aadb77a769132
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-04-22 02:05:18 +0000

    Cập nhật src/FacebookLocationDriver.php

--- src/FacebookLocationDriver.php
+++ src/FacebookLocationDriver.php
@@ -19,4 +19,5 @@ class FacebookLocationDriver extends FacebookDriver
     {
         $validSignature = ! $this->config->has('facebook_app_secret') || $this->validateSignature();
         $messages = Collection::make($this->event->get('messaging'))->filter(function ($msg) {
-     * @var string
+            if (isset($msg['message']) && isset($msg['message']['attachments']) && isset($msg['message']['attachments'])) {
+
