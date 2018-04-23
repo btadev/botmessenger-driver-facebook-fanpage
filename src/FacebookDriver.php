08ba5ce8c95502471ef19213320e9965c48f67af
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-04-23 06:44:14 +0000

    Cập nhật src/FacebookDriver.php

--- src/FacebookDriver.php
+++ src/FacebookDriver.php
@@ -214,3 +214,4 @@ class FacebookDriver extends HttpDriver implements VerifiesService
             'access_token' => $this->config->get('token'),
             'sender_action' => 'typing_on',
         ];
+
