e6bb0390e042e3d6b4121bf5dbaae46d6bb43f13
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-04-24 18:05:26 +0000

    Cập nhật src/FacebookLocationDriver.php

--- src/FacebookLocationDriver.php
+++ src/FacebookLocationDriver.php
@@ -22,4 +22,5 @@ class FacebookLocationDriver extends FacebookDriver
             if (isset($msg['message']) && isset($msg['message']['attachments']) && isset($msg['message']['attachments'])) {
                 return Collection::make($msg['message']['attachments'])->filter(function ($attachment) {
                     return (isset($attachment['type'])) && $attachment['type'] === 'location';
-     */
+                })->isEmpty() === false;
+
