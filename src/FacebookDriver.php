90806511969b5ec4ab0ee222643a83fcd8159a8c
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-05-04 23:41:03 +0000

    Cập nhật src/FacebookDriver.php

--- src/FacebookDriver.php
+++ src/FacebookDriver.php
@@ -377,3 +377,4 @@ class FacebookDriver extends HttpDriver implements VerifiesService
                     ],
                 ];
             } else {
+                $parameters['message']['text'] = $message->getText();
