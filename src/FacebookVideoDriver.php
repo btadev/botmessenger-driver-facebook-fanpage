f164b72ea07b718918e191b69af879dc8c77f611
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-05-06 10:04:01 +0000

    Cập nhật src/FacebookVideoDriver.php

--- src/FacebookVideoDriver.php
+++ src/FacebookVideoDriver.php
@@ -4,3 +4,4 @@ namespace ChienIT\BotMessenger\Drivers\Facebook;
 
 use Illuminate\Support\Collection;
 use ChienIT\BotMessenger\Messages\Attachments\Video;
+use ChienIT\BotMessenger\Messages\Incoming\IncomingMessage;
