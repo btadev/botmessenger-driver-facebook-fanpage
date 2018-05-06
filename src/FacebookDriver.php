fb4ca826cb34046e8dab91f21072b51d917dc4f5
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-05-06 05:10:56 +0000

    Cập nhật src/FacebookDriver.php

--- src/FacebookDriver.php
+++ src/FacebookDriver.php
@@ -192,3 +192,4 @@ class FacebookDriver extends HttpDriver implements VerifiesService
     {
         $parameters = [
             'recipient' => [
+                'id' => $matchingMessage->getSender(),
