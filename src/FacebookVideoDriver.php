3ccb9c482b74504ee9fe7b48618aae8986543a61
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-05-06 01:08:12 +0000

    Cập nhật src/FacebookVideoDriver.php

--- src/FacebookVideoDriver.php
+++ src/FacebookVideoDriver.php
@@ -61,4 +61,5 @@ class FacebookVideoDriver extends FacebookDriver
 
         if (count($messages) === 0) {
             $messages = [new IncomingMessage('', '', '')];
-
+        }
+    public function jsonSerialize()
