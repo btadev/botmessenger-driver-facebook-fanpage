d30f2461bdf6e69b0b54b864e507dad8e60471b5
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-05-07 09:45:09 +0000

    Cập nhật src/FacebookDriver.php

--- src/FacebookDriver.php
+++ src/FacebookDriver.php
@@ -203,3 +203,4 @@ class FacebookDriver extends HttpDriver implements VerifiesService
 
     /**
      * @param IncomingMessage $matchingMessage
+     * @return \Symfony\Component\HttpFoundation\Response
