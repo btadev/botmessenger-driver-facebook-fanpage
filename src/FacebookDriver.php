d59fd973bbcc72f65ae4e5a59071b48b9623f6e2
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-05-11 16:40:01 +0000

    Cập nhật src/FacebookDriver.php

--- src/FacebookDriver.php
+++ src/FacebookDriver.php
@@ -361,3 +361,4 @@ class FacebookDriver extends HttpDriver implements VerifiesService
          * If we send a Question with buttons, ignore
          * the text and append the question.
          */
+        if ($message instanceof Question) {
