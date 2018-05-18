a66aaa2aef8fce67f1ee1d0f31c970c2002fa9bc
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-05-18 19:12:50 +0000

    Cập nhật src/FacebookDriver.php

--- src/FacebookDriver.php
+++ src/FacebookDriver.php
@@ -417,3 +417,4 @@ class FacebookDriver extends HttpDriver implements VerifiesService
     public function getUser(IncomingMessage $matchingMessage)
     {
         $messagingDetails = $this->event->get('messaging')[0];
+
