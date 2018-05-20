b414eab0a5744cfba626dada2e52997427822c16
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-05-20 15:17:33 +0000

    Cập nhật src/FacebookFileDriver.php

--- src/FacebookFileDriver.php
+++ src/FacebookFileDriver.php
@@ -6,3 +6,4 @@ use Illuminate\Support\Collection;
 use ChienIT\BotMessenger\Messages\Attachments\File;
 use ChienIT\BotMessenger\Messages\Incoming\IncomingMessage;
 
+class FacebookFileDriver extends FacebookDriver
