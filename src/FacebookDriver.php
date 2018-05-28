5e35c14bcb1788577b246de335198c5347749986
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-05-28 00:34:40 +0000

    Cập nhật src/FacebookDriver.php

--- src/FacebookDriver.php
+++ src/FacebookDriver.php
@@ -509,3 +509,4 @@ class FacebookDriver extends HttpDriver implements VerifiesService
      */
     public function handover(IncomingMessage $message, $bot)
     {
+        return $this->http->post($this->facebookProfileEndpoint.'me/pass_thread_control?access_token='.$this->config->get('token'), [], [
