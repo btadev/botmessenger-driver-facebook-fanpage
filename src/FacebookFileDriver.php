9fd64c4c7b889f538f3d656204c2859581b9f2e1
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-06-04 19:24:13 +0000

    Cập nhật src/FacebookFileDriver.php

--- src/FacebookFileDriver.php
+++ src/FacebookFileDriver.php
@@ -4,3 +4,4 @@ namespace ChienIT\BotMessenger\Drivers\Facebook;
 
 use Illuminate\Support\Collection;
 use ChienIT\BotMessenger\Messages\Attachments\File;
+use ChienIT\BotMessenger\Messages\Incoming\IncomingMessage;
