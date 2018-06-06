16e52a37c2a11919f9d99da11bec7cfc22027c54
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-06-06 08:04:40 +0000

    Cập nhật src/FacebookImageDriver.php

--- src/FacebookImageDriver.php
+++ src/FacebookImageDriver.php
@@ -52,3 +52,4 @@ class FacebookImageDriver extends FacebookDriver
     {
         $messages = Collection::make($this->event->get('messaging'))->filter(function ($msg) {
             return isset($msg['message']) && isset($msg['message']['attachments']) && isset($msg['message']['attachments']);
+        })->transform(function ($msg) {
