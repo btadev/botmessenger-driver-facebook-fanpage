dedb7583bd5385f027c6ed84f7df1c41ca1e749d
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-06-16 00:19:22 +0000

    Cập nhật src/FacebookLocationDriver.php

--- src/FacebookLocationDriver.php
+++ src/FacebookLocationDriver.php
@@ -6,3 +6,4 @@ use Illuminate\Support\Collection;
 use ChienIT\BotMessenger\Messages\Attachments\Location;
 use ChienIT\BotMessenger\Messages\Incoming\IncomingMessage;
 
+class FacebookLocationDriver extends FacebookDriver
