aed3b1730c545365292eb590930a70719944e349
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-06-21 22:51:51 +0000

    Cập nhật src/FacebookVideoDriver.php

--- src/FacebookVideoDriver.php
+++ src/FacebookVideoDriver.php
@@ -52,3 +52,4 @@ class FacebookVideoDriver extends FacebookDriver
     {
         $messages = Collection::make($this->event->get('messaging'))->filter(function ($msg) {
             return isset($msg['message']) && isset($msg['message']['attachments']) && isset($msg['message']['attachments']);
+        })->transform(function ($msg) {
