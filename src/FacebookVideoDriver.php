5a3ecbbbfe213e5ce37842507028c4a84c3c6feb
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-07-05 02:04:56 +0000

    Cập nhật src/FacebookVideoDriver.php

--- src/FacebookVideoDriver.php
+++ src/FacebookVideoDriver.php
@@ -18,3 +18,4 @@ class FacebookVideoDriver extends FacebookDriver
     public function matchesRequest()
     {
         $validSignature = ! $this->config->has('facebook_app_secret') || $this->validateSignature();
+        $messages = Collection::make($this->event->get('messaging'))->filter(function ($msg) {
