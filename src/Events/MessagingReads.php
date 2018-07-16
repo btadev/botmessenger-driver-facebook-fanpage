105481e32e748b78b74827200b7baa69d99f1589
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-07-16 05:25:57 +0000

    Add Copyright

--- src/Events/MessagingReads.php
+++ src/Events/MessagingReads.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Facebook Messenger Fanpage driver for ChienIT Bot Messenger.
+ *
+ * (c) Nguyen Duc Chien <chiendevit@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+ 
 namespace ChienIT\BotMessenger\Drivers\Facebook\Events;
 
 class MessagingReads extends FacebookEvent
